a4730c01ba97833d55a399caa19a15a0a23044a7
Albin Marchand <albin.marchand@gmail.com>
2023-11-21 17:40:09 +0100

    responsive desktop input / image ok

--- components/RunningCalculator.vue
+++ components/RunningCalculator.vue
@@ -1,68 +1,75 @@
 <template>
   <div class="h-full flex flex-col justify-around">
-    <div class="px-6 h-2/3 flex flex-col justify-around">
-      <div class="text-center">
+    <div class="h-full flex flex-col justify-between lg:justify-start">
+      <div class="text-center px-6 pt-6">
         Calculez en un clin d’oeil votre vitesse moyenne et votre allure par
-        kilometre. Il vous suffit de rentrer la distance de course et le chonro
-        à réaliser et le tour est joué !
+        kilometre. <br />
+        Il vous suffit de rentrer la distance de course et le chonro à réaliser
+        et le tour est joué !
       </div>
-      <h1 class="text-2xl font-extrabold text-center">
+      <h1
+        class="text-2xl flex items-center justify-center font-extrabold text-center px-6 lg:h-48"
+      >
         Calculatrice d'allure de course
       </h1>
 
-      <div class="max-w-sm">
-        <label for="distance">Distance (km): </label>
-        <input
-          v-model="objParametersRunningCalculator.distance"
-          type="number"
-          step="0.01"
-          id="distance"
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        />
-      </div>
+      <div class="flex flex-col lg:max-h-100 mx-auto">
+        <div class="flex flex-col lg:flex-row lg:border-2 lg:rounded-xl">
+          <div class="h-80 flex flex-col justify-around px-6 lg:w-1/2">
+            <div class="max-w-sm">
+              <label for="distance">Distance (km): </label>
+              <input
+                v-model="objParametersRunningCalculator.distance"
+                type="number"
+                step="0.01"
+                id="distance"
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              />
+            </div>
 
-      <div class="flex gap-5 max-w-sm">
-        <div>
-          <label for="hours">Heures :</label>
-          <input
-            v-model="objParametersRunningCalculator.hours"
-            type="number"
-            id="hours"
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </div>
+            <div class="flex gap-5 max-w-sm">
+              <div>
+                <label for="hours">Heures :</label>
+                <input
+                  v-model="objParametersRunningCalculator.hours"
+                  type="number"
+                  id="hours"
+                  class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                />
+              </div>
 
-        <div>
-          <label for="minutes">Minutes :</label>
-          <input
-            v-model="objParametersRunningCalculator.minutes"
-            type="number"
-            id="minutes"
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </div>
+              <div>
+                <label for="minutes">Minutes :</label>
+                <input
+                  v-model="objParametersRunningCalculator.minutes"
+                  type="number"
+                  id="minutes"
+                  class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                />
+              </div>
 
-        <div>
-          <label for="seconds">Secondes :</label>
-          <input
-            v-model="objParametersRunningCalculator.seconds"
-            type="number"
-            id="seconds"
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </div>
-      </div>
+              <div>
+                <label for="seconds">Secondes :</label>
+                <input
+                  v-model="objParametersRunningCalculator.seconds"
+                  type="number"
+                  id="seconds"
+                  class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                />
+              </div>
+            </div>
 
-      <div class="w-full flex justify-center max-w-sm">
-        <button
-          @click="calculate"
-          class="bg-black text-white w-full h-14 text-2xl font-extrabold"
-        >
-          C'est parti !
-        </button>
-      </div>
+            <div class="w-full flex justify-center max-w-sm">
+              <button
+                @click="calculate"
+                class="bg-black text-white w-full h-14 text-2xl font-extrabold"
+              >
+                C'est parti !
+              </button>
+            </div>
+          </div>
 
-      <div
+          <!-- <div
         v-if="
           typeof objParametersRunningCalculator.result.speed === 'number' &&
           objParametersRunningCalculator.result.speed !== 0
@@ -79,16 +86,19 @@
           km/h
         </p>
         <p>Allure: {{ objParametersRunningCalculator.result.pace }}</p>
-      </div>
-    </div>
-    <div class="h-1/3 flex content-end relative">
-      <img
-        class="w-full h-auto object-cover"
-        src="@/assets/images/icons/runner_1.jpg"
-        alt=""
-      />
-      <div class="absolute bottom-0 left-0 p-5 text-white">
-        <h1 class="text-5xl font-extrabold">Unbreak.</h1>
+      </div> -->
+
+          <div class="flex content-end relative lg:w-1/2">
+            <img
+              class="w-full h-auto object-cover lg:rounded-tr-lg lg:rounded-br-lg"
+              src="@/assets/images/icons/runner_1.jpg"
+              alt=""
+            />
+            <div class="absolute bottom-0 left-0 p-5 text-white">
+              <h1 class="text-5xl font-extrabold">Unbreak.</h1>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
