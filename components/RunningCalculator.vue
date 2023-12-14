e7841b154644d2f50eb6e83fbd81d0c72feaa01e
Albin Marchand <albin.marchand@gmail.com>
2023-12-14 15:12:18 +0100

    Fix reponsive tablet

--- components/RunningCalculator.vue
+++ components/RunningCalculator.vue
@@ -5,18 +5,30 @@
         <p
           class="text-justify px-6 pt-5 pb-5 md:w-4/5 md:w2/3 lg:flex lg:flex-col lg:items-center lg:h-28 lg:content-around lg:justify-around"
         >
-          <span>Calculez en un clin d’oeil votre vitesse moyenne et votre allure par kilometre.</span>
-          <span>Il vous suffit de rentrer la distance de course et le chonro à réaliser et le tour est joué !</span>
+          <span
+            >Calculez en un clin d’oeil votre vitesse moyenne et votre allure
+            par kilometre.</span
+          >
+          <span
+            >Il vous suffit de rentrer la distance de course et le chonro à
+            réaliser et le tour est joué !</span
+          >
         </p>
       </div>
-      <h1 class="text-2xl flex items-center justify-center font-extrabold text-center px-6 lg:h-32">
+      <h1
+        class="text-2xl flex items-center justify-center font-extrabold text-center px-6 lg:h-32"
+      >
         Calculatrice d'allure de course
       </h1>
 
       <div class="flex flex-col lg:max-h-100 mx-auto items-center">
-        <div class="flex flex-col lg:flex-row lg:border-2 lg:rounded-xl lg:w-9/12">
-          <div class="h-80 flex flex-col justify-around px-6 items-center lg:w-1/2 lg:h-full">
-            <div class="w-full md:w-96 lg:w-96">
+        <div
+          class="flex flex-col lg:flex-row lg:border-2 lg:rounded-xl lg:w-9/12 lg:min-w-[90%] lg:max-w-screen-xl"
+        >
+          <div
+            class="h-80 flex flex-col justify-around px-6 items-center lg:w-1/2 lg:h-full"
+          >
+            <div class="w-full max-w-sm lg:w-96">
               <label for="distance">Distance (km): </label>
               <input
                 @click="ResetDistance()"
@@ -64,22 +76,31 @@
             </div>
 
             <div class="w-full flex justify-center max-w-sm">
-              <button @click="calculate" class="bg-black text-white w-full h-14 text-2xl font-extrabold">
+              <button
+                @click="calculate"
+                class="bg-black text-white w-full h-14 text-2xl font-extrabold"
+              >
                 C'est parti !
               </button>
             </div>
             <div
               class="grid grid-flow-col gap-2 w-full md:grid-rows-1 md:my-4"
               v-if="
-                typeof objParametersRunningCalculator.result.speed === 'number' &&
-                objParametersRunningCalculator.result.speed !== 0
+                typeof objParametersRunningCalculator.result.speed ===
+                  'number' && objParametersRunningCalculator.result.speed !== 0
               "
             >
               <div class="">
-                <div class="bg-black text-white flex items-center justify-center">Vitesse moyenne :</div>
+                <div
+                  class="bg-black text-white flex items-center justify-center"
+                >
+                  Vitesse moyenne :
+                </div>
                 <div class="text-2xl text-center font-extrabold">
                   {{
-                    isNaN(objParametersRunningCalculator.result.speed.toFixed(2))
+                    isNaN(
+                      objParametersRunningCalculator.result.speed.toFixed(2)
+                    )
                       ? 0
                       : objParametersRunningCalculator.result.speed.toFixed(2)
                   }}
@@ -88,7 +109,11 @@
               </div>
 
               <div class="">
-                <div class="bg-black text-white flex items-center justify-center">Allure :</div>
+                <div
+                  class="bg-black text-white flex items-center justify-center"
+                >
+                  Allure :
+                </div>
                 <div class="text-2xl text-center font-extrabold">
                   {{ objParametersRunningCalculator.result.pace }}
                 </div>
@@ -127,7 +152,9 @@ const objParametersRunningCalculator = ref({
 });
 
 function calculate() {
-  for (const [key, value] of Object.entries(objParametersRunningCalculator.value)) {
+  for (const [key, value] of Object.entries(
+    objParametersRunningCalculator.value
+  )) {
     value === ""
       ? (objParametersRunningCalculator.value[key] = 0)
       : (objParametersRunningCalculator.value[key] = value);
@@ -143,7 +170,10 @@ function calculate() {
   const paceSeconds = Math.round((pace - paceMinutes) * 60);
   objParametersRunningCalculator.value.result = {
     speed: objParametersRunningCalculator.value.distance / (totalMinutes / 60),
-    pace: isNaN(paceMinutes) && isNaN(paceSeconds) ? 0 + "min/km" : `${paceMinutes}'${paceSeconds}" min/km`,
+    pace:
+      isNaN(paceMinutes) && isNaN(paceSeconds)
+        ? 0 + "min/km"
+        : `${paceMinutes}'${paceSeconds}" min/km`,
   };
 }
 function ResetDistance() {
@@ -160,7 +190,10 @@ function ResetSeconds() {
 }
 onMounted(() => {
   console.log(process.env.NODE_ENV);
-  console.log("objParametersRunningCalculator => ", objParametersRunningCalculator.value);
+  console.log(
+    "objParametersRunningCalculator => ",
+    objParametersRunningCalculator.value
+  );
   console.log("Store =>", paces.paces);
 });
 </script>
