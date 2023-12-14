74a3f3891015f56764f4637fa0812f1af3d4d9d0
Albin Marchand <albin.marchand@gmail.com>
2023-12-14 16:43:08 +0100

    setup sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,12 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-  modules: ["@pinia/nuxt", "nuxt-gtag"],
+  modules: ["@pinia/nuxt", "nuxt-gtag", "nuxt-simple-sitemap"],
   gtag: {
     id: "GTM-5M9SPTJX",
+  },
+  site: {
+    url: "https://unbreak-sport.com",
   },
   postcss: {
     plugins: {
