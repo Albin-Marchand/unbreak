f36de1b6389f5aa133d6b186566f83b53fb6f330
Albin Marchand <albin.marchand@gmail.com>
2024-01-04 11:58:31 +0100

    jout Google Tag + Modif resultat

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,6 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
   modules: ["@pinia/nuxt", "nuxt-simple-sitemap"],
-  runtimeConfig: {
-    public: {
-      gtagId: "G-EKBV2R1LK6",
-    },
-  },
-
   site: {
     url: "https://unbreak-sport.com",
   },
