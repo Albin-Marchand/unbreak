18beca86985920b53b7f751c847e90d13f5de867
Albin Marchand <albin.marchand@gmail.com>
2024-01-11 13:24:16 +0100

    Calculette Vitesse / Temps moyen fonctionelle

--- components/RunningVitesseCalculator.vue
+++ components/RunningVitesseCalculator.vue
@@ -1,11 +1,66 @@
 <template>
-  <div class="h-full flex flex-col">VITESSE MOYENNE EN COURS DE DEVELOPPEMENT ...</div>
+  <div>
+    <h2>Calculateur de Temps de Course</h2>
+
+    <div>
+      <label for="paceMinutes">Allure Moyenne (minutes): </label>
+      <input type="number" v-model="paceMinutes" />
+    </div>
+
+    <div>
+      <label for="paceSeconds">Allure Moyenne (secondes): </label>
+      <input type="number" v-model="paceSeconds" />
+    </div>
+
+    <div>
+      <label for="distance">Distance (km): </label>
+      <input type="number" v-model="distance" />
+    </div>
+
+    <button @click="calculateTime">Calculer</button>
+
+    <div v-if="result">
+      <h3>Résultat :</h3>
+      <p>
+        {{ result.hours }} heures, {{ result.minutes }} minutes,
+        {{ result.seconds }} secondes
+      </p>
+      <p>Vitesse Moyenne: {{ result.speed.toFixed(2) }} km/h</p>
+    </div>
+  </div>
 </template>
 
-<script setup>
-import { ref, onMounted } from "vue";
+<script>
+export default {
+  data() {
+    return {
+      paceMinutes: 0,
+      paceSeconds: 0,
+      distance: 0,
+      result: null,
+    };
+  },
+  methods: {
+    calculateTime() {
+      const paceInSeconds = this.paceMinutes * 60 + this.paceSeconds; // Convert average pace to seconds per kilometer
+      const totalSeconds = paceInSeconds * this.distance; // Calculate total time in seconds
+
+      // Convert total seconds to hours, minutes, and remaining seconds
+      const hours = Math.floor(totalSeconds / 3600);
+      const minutes = Math.floor((totalSeconds % 3600) / 60);
+      const seconds = Math.floor(totalSeconds % 60);
+
+      this.result = { hours, minutes, seconds };
+
+      const totalHours = totalSeconds / 3600;
+      const speed = this.distance / totalHours;
+
+      this.result = { hours, minutes, seconds, speed };
+    },
+  },
+};
 </script>
 
 <style scoped>
-/* CSS */
+/* Ajoutez des styles CSS au besoin */
 </style>
